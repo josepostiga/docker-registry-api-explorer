6cd7a685d99ae6fe4562bb7dc5a2e1f8399651be
josepostiga <josepostiga1990@gmail.com>
2019-07-21 21:27:18 +0000

    Apply fixes from StyleCI

--- src/DockerRegistryServiceProvider.php
+++ src/DockerRegistryServiceProvider.php
@@ -10,13 +10,13 @@ class DockerRegistryServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../config/config.php' => config_path('docker-registry.php'),
+            __DIR__.'/../config/config.php' => config_path('docker-registry.php'),
         ], 'config');
     }
 
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'docker-registry');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'docker-registry');
 
         $this->app->singleton(DockerRegistryApiClient::class, function () {
             return new DockerRegistryApiClient(config('docker-registry.url'), config('docker-registry.port'));
